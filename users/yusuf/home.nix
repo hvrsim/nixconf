ea283f28f433058aea1b51c12ebb508726f068f1
Yusuf M <circutrider21@outlook.com>
2024-12-24 16:37:27 -0600

    hm: nixvim package imported

--- users/yusuf/home.nix
+++ users/yusuf/home.nix
@@ -1,11 +1,20 @@
-{pkgs, ...}: {
+{pkgs, nixvim, ...}: {
   imports = [
+    nixvim.homeManagerModules.nixvim
     ../../home/core.nix
   ];
 
-  programs.git = {
-    enable = true;
-    userName = "Yusuf M";
-    userEmail = "circutrider21@outlook.com";
+  programs = {
+    git = {
+      enable = true;
+      userName = "Yusuf M";
+      userEmail = "circutrider21@outlook.com";
+    };
+
+    nixvim = {
+      enable = true;
+      defaultEditor = true;
+      vimAlias = true;
+    };
   };
 }
