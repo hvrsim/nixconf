495d1d9298bb37c41db78e281e9f4eca686b1f93
Yusuf M <circutrider21@outlook.com>
2024-12-25 20:25:55 -0600

    thinkpad: improved hardware support

--- hosts/thinkpad/configuration.nix
+++ hosts/thinkpad/configuration.nix
@@ -4,8 +4,11 @@ let
   inherit (builtins) attrValues;
 in
 {
-  imports = attrValues nixconf.nixosModules;
+  imports = attrValues nixconf.nixosModules ++ [nixconf.inputs.nixos-hardware.nixosModules.lenovo-thinkpad-x250];
   home-manager.sharedModules = attrValues nixconf.homeModules;
+
+  # enable microcode updates.
+  hardware.cpu.intel.updateMicrocode = true;
 
   modules = {
     hardware.bluetooth = true;
